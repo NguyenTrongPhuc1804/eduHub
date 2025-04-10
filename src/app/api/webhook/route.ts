3ffa2b58f1f384527e7fd87eb62b18313a15af0a
NguyenTrongPhuc <phucnguyentrong1804@gmail.com>
2025-04-10 16:19:17 +0700

    remove is defined but never used

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,6 +1,5 @@
 import CreateUser from "@/lib/action/user.action";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
